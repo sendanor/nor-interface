69338769afa232bfa65d115bb46ac2768107acf8
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-04-26 17:20:18 +0300

    Initial version

--- src/parse-form.js
+++ src/parse-form.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+var _Q = require('q');
 var tv4 = require('tv4');
 var $ = require('jquery');
 var copy = require('nor-data').copy;
@@ -88,74 +89,95 @@ function change_property(data, key, value) {
 	obj[name] = value;
 }
 
-/** Convert form values into JavaScript object */
+/** Convert form values into JavaScript object
+ * @param opts.types {object} NoPg object types
+ * @param opts.postProcessing {Function|Promise} Called after initial data has been fetched from the form and before JSON schema validation.
+ * @returns {object} A promise of the final data object
+ */
 var parse_form = module.exports = function parse_form(form, opts) {
-
 	opts = opts || {};
 	debug.assert(opts).is('object');
 
 	opts.types = opts.types || {};
 	debug.assert(opts.types).is('object');
 
-	var data = {};
+	var tmp;
 	var input_map = {};
 	var nopg_type = $(form).attr('data-nopg-type');
-	//debug.log('nopg_type = ', nopg_type);
 
-	// Read input elements
-	$(form).find('input, select, textarea').each(function() {
-		var id = $(this).attr('id');
-		var type = $(this).attr('type');
-		var key = $(this).attr('name') || id;
-		var value = $(this).val();
-		input_map[key] = $(this);
+	return _Q.fcall(function() {
 
-		$(this).removeClass("input-error");
-			
-		var default_datatype = 'string';
-			
-		if(type === 'checkbox') {
-			default_datatype = 'boolean';
-			value = $(this).is(':checked');
-		}
+		var data = {};
 
-		var datatype = $(this).attr('data-type') || default_datatype;
+		//debug.log('nopg_type = ', nopg_type);
+
+		// Read input elements
+		$(form).find('input, select, textarea').each(function handle_input_elements() {
+			var id = $(this).attr('id');
+			var type = $(this).attr('type');
+			var key = $(this).attr('name') || id;
+			var value = $(this).val();
+			input_map[key] = $(this);
 	
-		if(typeof converters[datatype] === 'function') {
-			value = converters[datatype](value);
-		} else {
-			value = converters.string(value);
-		}
+			$(this).removeClass("input-error");
+			
+			var default_datatype = 'string';
+				
+			if(type === 'checkbox') {
+				default_datatype = 'boolean';
+				value = $(this).is(':checked');
+			}
+	
+			var datatype = $(this).attr('data-type') || default_datatype;
+		
+			if(typeof converters[datatype] === 'function') {
+				value = converters[datatype](value);
+			} else {
+				value = converters.string(value);
+			}
 
-		// Handle object properties
-		if(key && (key.indexOf('.') >= 0) ) {
-			change_property(data, key, value);
-		} else if(key) {
-			data[key] = value;
-		} else {
-			debug.warn('Warning! No key detected for value: ', value);
+			// Handle object properties
+			if(key && (key.indexOf('.') >= 0) ) {
+				change_property(data, key, value);
+			} else if(key) {
+				data[key] = value;
+			} else {
+				debug.warn('No key detected for value: ', value);
+			}
+		});
+
+		return data;
+	}).then(function(data) {
+
+		// User defined custom checks and modifications
+		return _Q.when(opts.postProcessing).then(function(postProcessing) {
+			if(is.func(postProcessing)) {
+				return _Q.when(postProcessing(data)).then(function(tmp) {
+					if(tmp === undefined) {
+						return data;
+					}
+					return tmp;
+				});
+			}
+			return data;
+		});
+
+	}).then(function(data) {
+		// JSON schema validation
+		if(nopg_type && is.obj(opts.types[nopg_type]) && (opts.types[nopg_type].$schema !== undefined)) {
+			//debug.log( "nopg_type = ", nopg_type );
+			//debug.log( "opts.types[nopg_type] = ", opts.types[nopg_type] );
+			if(toggle_error_classes(data, opts.types[nopg_type].$schema, input_map)) {
+				debug.log('schema =' , opts.types[nopg_type].$schema);
+				debug.log('data =' , data);
+				throw new TypeError("JSON Schema validation failed");
+			}
 		}
+	
+		// FIXME: Implement support for .$validate
+		
+		return data;
 	});
-
-	// User defined custom checks and modifications
-	if(opts && opts.postProcessing && (typeof opts.postProcessing === 'function') ) {
-		data = opts.postProcessing(data);
-	}
-	
-	// JSON schema validation
-	if(nopg_type && is.obj(opts.types[nopg_type]) && (opts.types[nopg_type].$schema !== undefined)) {
-		//debug.log( "nopg_type = ", nopg_type );
-		//debug.log( "opts.types[nopg_type] = ", opts.types[nopg_type] );
-		if(toggle_error_classes(data, opts.types[nopg_type].$schema, input_map)) {
-			debug.log('schema =' , opts.types[nopg_type].$schema);
-			debug.log('data =' , data);
-			throw new TypeError("JSON Schema validation failed");
-		}
-	}
-
-	// FIXME: Implement support for .$validate
-	
-	return data;
 };
 
 /* EOF */
